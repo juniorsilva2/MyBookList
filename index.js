bca6b4a8787bb856b7c678409c95bec25fb50f0f
Junior <junior_silva158@hotmail.com>
2022-08-09 16:49:04 -0300

    Passando o back para MVC e adicionando o front

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const dotenv = require("dotenv");
-const connectToDatabase = require("./src/database/connect");
-dotenv.config();
+const connectToDatabase = require("./src/database/mongodb");
+const express = require("express");
+const path = require("path");
+const bookController = require("./src/controllers/bookController");
+const BookModel = require("./src/models/bookModel");
+const app = express();
+const bodyParser = require('body-parser');
 
-require("./modules/express");
+dotenv.config();
 connectToDatabase();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "src/public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Renderiza a página principal
+app.get("/", bookController.buscarLivros);
+
+// Renderiza a página de adicionar livro
+app.get("/novoLivro", bookController.novoLivro);
+
+// Renderiza a página para atualizar e deletar o livro
+app.get("/Livro/:id", bookController.inspecionaLivro);
+
+// Adiciona um livro
+app.post("/addLivro", bookController.adicionarLivro);
+
+// Atualiza os dados de um livro
+// app.put("/updateLivro/:id", bookController.atualizarLivro);
+app.post("/updateLivro/:id", bookController.atualizarLivro);
+
+// Deleta um livro
+// app.delete("/deleteLivro/:id", bookController.deletarLivro);
+app.get("/deleteLivro/:id", bookController.deletarLivro);
+
+app.listen(process.env.EXPRESS_PORT, () =>
+  console.log(
+    `Rodando na URL: http://${process.env.EXPRESS_HOST}:${process.env.EXPRESS_PORT}`
+  )
+);
---
+++ src/controllers/bookController.js
@@ -0,0 +1,81 @@
+const BookModel = require("../models/bookModel");
+
+// Renderiza a página principal
+const buscarLivros = async (req, res) => {
+  try {
+    const books = await BookModel.find();
+    res.status(200).render("home", { books });
+  } catch (error) {
+    res.status(404).send(error);
+  }
+};
+
+// Renderiza a página de adicionar livro
+const novoLivro = async (req, res) => {
+  try {
+    res.status(200).render("insert");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+// Renderiza a página para atualizar e deletar o livro
+const inspecionaLivro = async (req, res) => {
+  try {
+    const book = await BookModel.findById(req.params.id);
+    if (book === null) {
+      res.status(404).send("Livro não encontrado");
+    } else {
+      res.status(200).render("update", { book });
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+// Adiciona um livro
+const adicionarLivro = async (req, res) => {
+  try {
+    const book = await BookModel.create(req.body);
+    res.status(200).redirect("/");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+// Atualiza os dados de um livro
+const atualizarLivro = async (req, res) => {
+  try {
+    const book = await BookModel.findByIdAndUpdate(req.params.id, req.body);
+    if(book === null){
+      res.status(404).send("Livro não encontrado");
+    } else {
+      res.status(200).redirect("/");
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+// Deleta um livro
+const deletarLivro = async (req, res) => {
+  try {
+    const book = await BookModel.findByIdAndRemove(req.params.id);
+    if (book === null) {
+      res.status(404).send("Livro não encontrado");
+    } else {
+      res.status(200).redirect("/");
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = {
+  adicionarLivro,
+  buscarLivros,
+  inspecionaLivro,
+  atualizarLivro,
+  deletarLivro,
+  novoLivro,
+};
